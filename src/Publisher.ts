eee9d2a226ae76daa04398a93d32ccb0057b54da
tongtwist <arnaud.dussart@gmail.com>
2025-01-18 17:07:21 +0100

    Some simplifications and comments

--- src/Publisher.ts
+++ src/Publisher.ts
@@ -1,69 +1,102 @@
 import {
-  type ISubscription,
+  type Topic,
   type TopicPattern,
   type ConsumeFunction,
-  type ContentFilterFunction,
+  type ContentFilteringFunction,
   type MaybePromise,
   Subscription,
 } from "./Subscription";
 
-export interface IPublisher<Msg> {
-  subscribe(
-    consume: ConsumeFunction<Msg>,
-    topic?: TopicPattern,
-    contentFilter?: ContentFilterFunction<Msg>,
-    strictTopicPatternFiltering?: boolean
-  ): Unsubscribe;
-  readonly subscriptionsNumber: number;
-}
-
+/**
+ * Function to cancel a subscription.
+ * @callback Unsubscribe
+ * @returns {void}
+ */
 export type Unsubscribe = () => void;
 
-export interface IEmitter<Msg> {
-  emit(message: Msg, topic?: TopicPattern): MaybePromise<void>;
-}
+/**
+ * Function type for emitting messages to subscribers.
+ * @template Msg - Type of messages that can be emitted
+ * @param {Msg} msg - The message to emit
+ * @param {Topic} [topic] - Optional topic associated with the message
+ * @returns {MaybePromise<void>} Promise that resolves when all subscribers have processed the message
+ */
+export type Emitter<Msg> = (msg: Msg, topic?: Topic) => MaybePromise<void>;
 
-export class Publisher<Msg> implements IPublisher<Msg>, IEmitter<Msg> {
-  private readonly subscriptions: Map<ISubscription<Msg>, Unsubscribe>;
+/**
+ * Class managing message subscriptions.
+ * Provides functionality for subscribing to messages and emitting messages to subscribers.
+ * @template Msg - Type of messages handled by the publisher
+ */
+export class Publisher<Msg> {
+  /**
+   * Map storing active subscriptions and their unsubscribe functions.
+   * @private
+   * @type {Map<Subscription<Msg>, Unsubscribe>}
+   */
+  private readonly subscriptions: Map<Subscription<Msg>, Unsubscribe>;
 
+  /**
+   * Private constructor to enforce factory pattern creation through create() method.
+   * @private
+   */
   private constructor() {
-    this.subscriptions = new Map<ISubscription<Msg>, Unsubscribe>();
+    this.subscriptions = new Map<Subscription<Msg>, Unsubscribe>();
   }
 
-  get subscriptionsNumber() {
+  /**
+   * Gets the current number of active subscriptions.
+   * @returns {number} The number of active subscriptions
+   */
+  get subscriptionsNumber(): number {
     return this.subscriptions.size;
   }
 
-  emit(msg: Msg, topic?: string): MaybePromise<void> {
-    const consumptionFunctionsToWait = this.subscriptions
-      .keys()
-      .reduce(
-        (acc: Promise<void>[], sub: ISubscription<Msg>): Promise<void>[] => {
-          if (sub.filter(msg, topic)) {
-            const consumptionRet = sub.consume(msg);
-            if (typeof consumptionRet !== "undefined") {
-              return acc.concat([consumptionRet]);
+  /**
+   * Returns a function that can be used to emit messages to subscribers.
+   * Useful when you want to pass emission capability without exposing the entire publisher.
+   * @returns {Emitter<Msg>} A function that can emit messages to subscribers
+   */
+  getEmitter(): Emitter<Msg> {
+    return (msg: Msg, topic?: Topic): MaybePromise<void> => {
+      const consumptionFunctionsToWait = this.subscriptions
+        .keys()
+        .reduce(
+          (acc: Promise<void>[], sub: Subscription<Msg>): Promise<void>[] => {
+            if (sub.check(msg, topic)) {
+              const consumptionRet = sub.consume(msg);
+              if (typeof consumptionRet !== "undefined") {
+                return acc.concat([consumptionRet]);
+              }
             }
-          }
-          return acc;
-        },
-        []
-      );
-    if (consumptionFunctionsToWait.length > 0) {
-      return Promise.all(consumptionFunctionsToWait).then(() => {});
-    }
+            return acc;
+          },
+          []
+        );
+      if (consumptionFunctionsToWait.length > 0) {
+        return Promise.all(consumptionFunctionsToWait).then(() => {});
+      }
+    };
   }
 
+  /**
+   * Registers a new subscription with optional filtering criteria.
+   * @param {ConsumeFunction<Msg>} consume - Function to handle matching messages
+   * @param {TopicPattern} [topicPattern] - Optional pattern to filter messages by topic
+   * @param {ContentFilteringFunction<Msg>} [contentFilter] - Optional function to filter messages by content
+   * @param {boolean} [strictTopicPatternFiltering=true] - If true, messages without topic will be rejected when a topicPattern is defined
+   * @returns {Unsubscribe} Function to cancel the subscription
+   */
   subscribe(
     consume: ConsumeFunction<Msg>,
-    topic?: TopicPattern,
-    contentFilter?: ContentFilterFunction<Msg>,
+    topicPattern?: TopicPattern,
+    contentFilter?: ContentFilteringFunction<Msg>,
     strictTopicPatternFiltering: boolean = true
   ): Unsubscribe {
-    const subscription: ISubscription<Msg> = new Subscription(
+    const subscription: Subscription<Msg> = new Subscription(
       consume,
       strictTopicPatternFiltering,
-      topic,
+      topicPattern,
       contentFilter
     );
     if (this.subscriptions.has(subscription)) {
@@ -74,7 +107,13 @@ export class Publisher<Msg> implements IPublisher<Msg>, IEmitter<Msg> {
     return unsubscribe;
   }
 
-  static create<Msg>(): IPublisher<Msg> & IEmitter<Msg> {
+  /**
+   * Creates a new Publisher instance.
+   * @template Msg - Type of messages to be handled by the publisher
+   * @returns {Publisher<Msg>} A new Publisher instance
+   * @static
+   */
+  static create<Msg>(): Publisher<Msg> {
     return new Publisher<Msg>();
   }
 }
--- src/Subscription.ts
+++ src/Subscription.ts
@@ -1,42 +1,99 @@
-export interface ISubscription<Msg = unknown> {
-  readonly topic?: TopicPattern;
-  readonly strictTopicFiltering: boolean;
-  readonly consume: ConsumeFunction<Msg>;
-  readonly contentFilter?: ContentFilterFunction<Msg>;
-  filter(msg: Msg, topic?: string | number): boolean;
-}
+/**
+ * Represents a message topic. It can be either a string or a number.
+ * @typedef {(string|number)} Topic
+ */
+export type Topic = string | number;
 
-export type TopicPattern = string | RegExp | number;
-export type ConsumeFunction<I = unknown> = (data: I) => MaybePromise<void>;
-export type ContentFilterFunction<I = unknown> = (data: I) => boolean;
+/**
+ * Pattern used to filter topics.
+ * Can be either an exact topic value (Topic) or a regular expression (RegExp).
+ * If the pattern is a RegExp and the topic is a number, the topic is converted to a string
+ * to allow matching with the regular expression.
+ * @typedef {(Topic|RegExp)} TopicPattern
+ */
+export type TopicPattern = Topic | RegExp;
+
+/**
+ * Consumer function called when a message passes the filters.
+ * @template Msg - Type of message to consume
+ * @callback ConsumeFunction
+ * @param {Msg} msg - The message data to consume
+ * @returns {MaybePromise<void>} Void or a Promise<void>
+ */
+export type ConsumeFunction<Msg = unknown> = (msg: Msg) => MaybePromise<void>;
+
+/**
+ * Function for filtering message content.
+ * Allows filtering messages in addition to topic filtering.
+ * @template Msg - Type of message to filter
+ * @callback ContentFilteringFunction
+ * @param {Msg} msg - The message data to filter
+ * @returns {boolean} true if the message should be kept, false otherwise
+ */
+export type ContentFilteringFunction<Msg = unknown> = (msg: Msg) => boolean;
+
+/**
+ * Utility type representing either a value T or a Promise of T.
+ * @template T - Type of the value
+ * @typedef {(T|Promise<T>)} MaybePromise
+ */
 export type MaybePromise<T> = T | Promise<T>;
 
-export class Subscription<Msg = unknown> implements ISubscription<Msg> {
-  private topicMatcher?: (givenTopic: string | number) => boolean;
+/**
+ * Class representing a subscription to messages.
+ * Handles the checks used in message filtering by topic and/or content.
+ * @template Msg - Type of messages handled by the subscription
+ */
+export class Subscription<Msg = unknown> {
+  /**
+   * Matching function for topic filtering.
+   * @private
+   * @type {((givenTopic: Topic) => boolean) | undefined}
+   */
+  private topicMatcher?: (givenTopic: Topic) => boolean;
 
+  /**
+   * Creates a new subscription.
+   * @param {ConsumeFunction<Msg>} consume - Function called to consume filtered messages
+   * @param {boolean} strictTopicFiltering - If true, messages without topic will be rejected when a topicPattern is defined
+   * @param {TopicPattern} [topicPattern] - Optional pattern to filter messages by topic
+   * @param {ContentFilteringFunction<Msg>} [contentFilter] - Optional function to filter messages by content
+   */
   constructor(
     public readonly consume: ConsumeFunction<Msg>,
     public readonly strictTopicFiltering: boolean,
-    public readonly topic?: TopicPattern,
-    public readonly contentFilter?: ContentFilterFunction<Msg>
+    public readonly topicPattern?: TopicPattern,
+    public readonly contentFilter?: ContentFilteringFunction<Msg>
   ) {
-    if (typeof topic !== "undefined") {
+    if (typeof topicPattern !== "undefined") {
       this.topicMatcher =
-        typeof topic === "string" || typeof topic === "number"
-          ? (givenTopic: string | number) => givenTopic === topic!
-          : (givenTopic: string | number) =>
-              givenTopic.toString().match(topic) !== null;
+        typeof topicPattern === "string" || typeof topicPattern === "number"
+          ? (givenTopic: Topic) => givenTopic === topicPattern!
+          : (givenTopic: Topic) =>
+              givenTopic.toString().match(topicPattern) !== null;
     }
   }
 
-  filter(msg: Msg, topic?: string | number): boolean {
+  /**
+   * Checks if a message matches the subscription's filtering criteria.
+   * @param {Msg} msg - The message to filter
+   * @param {Topic} [topic] - The optional topic associated with the message
+   * @returns {boolean} true if the message passes the filters, false otherwise
+   */
+  check(msg: Msg, topic?: Topic): boolean {
     return (
       this.matchTopic(topic) && (!this.contentFilter || this.contentFilter(msg))
     );
   }
 
-  private matchTopic(topic: string | number | undefined): boolean {
-    if (typeof this.topic === "undefined") {
+  /**
+   * Checks if a topic matches the filtering pattern.
+   * @private
+   * @param {Topic} [topic] - The topic to check
+   * @returns {boolean} true if the topic matches the pattern or if no pattern is defined
+   */
+  private matchTopic(topic?: Topic): boolean {
+    if (typeof this.topicPattern === "undefined") {
       return true;
     }
     if (typeof topic === "undefined") {
